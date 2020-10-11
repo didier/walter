ae0d4d960b5094e20f45bbb36b29549fbb59aa39
Didier Catz <didierlennon@gmail.com>
2020-10-11 02:45:34 +0200

    Add initial styling, make drink funtion

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,38 @@
 <script>
+  import Logo from './components/atoms/Logo.svelte'
+  import { tap } from '@sveltejs/gestures'
+  let current = 0
+  let total = 8
+
+  const drinkWater = () => {
+    if (current < total) {
+      current = current + 1
+    }
+  }
 </script>
 
-<style>
-	/* @import url('./styles/tailwind.css'); */
-	/* @import url('./styles/style.css'); */
-</style>
+<header
+  class="fixed w-screen px-8 py-8 grid gap-1 grid-flow-col-dense items-start justify-start">
+  <Logo />
+  <p class="font-semibold">Walter</p>
+</header>
+<main class="flex flex-col justify-center items-center h-screen w-screen">
+  <h4>You've had</h4>
+  <h1 class="mx-auto my-4 text-6xl px-8 text-center">{current}/{total}</h1>
+  <h4>glasses of water today</h4>
+</main>
 
-<h1>Yooo</h1>
+<footer
+  class="fixed bottom-0 left-0 w-screen px-8 py-8 flex items-center justify-center">
+  <nav>
+    <ul>
+      <li>
+        <button
+          on:click={drinkWater}
+          class="bg-teal-800 rounded-full -translate-y-32 inline-block p-6 shadow-xl transition-all duration-75 hover:bg-teal-700">
+          <Logo />
+        </button>
+      </li>
+    </ul>
+  </nav>
+</footer>
---
+++ src/components/atoms/Logo.svelte
@@ -0,0 +1,15 @@
+<svg
+  width="22"
+  height="22"
+  viewBox="0 0 22 22"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <path
+    d="M0 0L4.83918 21.1714H9.32378V19H6.57029L2.72376 2.17143H9.32378V0H0Z"
+    fill="currentColor" />
+  <path
+    fill-rule="evenodd"
+    clip-rule="evenodd"
+    d="M21.7238 0L16.8846 21.1714H11.3143V0H21.7238ZM15.1535 19L19 2.17143H13.4857V19H15.1535Z"
+    fill="currentColor" />
+</svg>
